0dc7cc8107dc21da525688f763cd0045acce5458
Sundram1343 <sundramsharma2901@gmail.com>
2026-03-28 18:43:24 +0530

    initial commit

---
+++ src/Components/NavBar/Navabr.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import { FaUser } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
+function Navbar() {
+    const [Active,setActive] =useState('Home')
+  return (
+    <>
+        <div id="Header">
+            <h1 id="Header-Title">EcoQuest</h1>
+            <div id='Header-Elements'>
+                <a id={Active==='Home'?'ActiveElement':'NonActiveElement'} onClick={()=>setActive('Home')}>Home</a>
+                <a id={Active==='Tasks'?'ActiveElement':'NonActiveElement'} onClick={()=>setActive('Tasks')}>Tasks</a>
+                <a id={Active==='LeaderBoard'?'ActiveElement':'NonActiveElement'} onClick={()=>setActive('LeaderBoard')}>LeaderBoard</a>
+                <a id={Active==='Impact'?'ActiveElement':'NonActiveElement'} onClick={()=>setActive('Impact')}>Impact</a>
+            </div>
+            <div id='header-last-element'>
+                <a id='PointsButton'>Points</a>
+                <FaBell id='Bell' />
+                <div id='user'>
+                    <span id='Username'>username</span>
+                    <FaUser id='usericon'/>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../Components/NavBar/Navabr'
+function Home() {
+  return (
+    <div><Navbar/></div>
+  )
+}
+
+export default Home
