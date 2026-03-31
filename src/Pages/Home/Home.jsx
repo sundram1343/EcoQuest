f907b33e49545620a1f28db810ac9740aa58a6d5
Sundram1343 <sundramsharma2901@gmail.com>
2026-04-01 02:22:15 +0530

    working on HomePage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.css'
 import Navbar from '../../Components/NavBar/Navabr'
 import { useAuth } from '../../context/AuthContext'
 import HomeImage from '../../assets/HomeImage.png'
+import { FaRocket } from "react-icons/fa";
+import { PiRankingLight } from "react-icons/pi"
+import { LuTrees } from "react-icons/lu";
 function Home() {
   const {authUser} =useAuth();
   return (
@@ -22,6 +25,28 @@ function Home() {
           </div>
           </div>
           <img src={HomeImage} id='HomeImage'/>
+        </div>
+      </div>
+      <div id='CentralContainer'>
+        <div id='GrowthBox'>
+          <div id='RocketContainer'>
+            <FaRocket id='RocketIcon'/>
+          </div>
+          <span id='GrowthTitle'>Start Challenge</span>
+          <span id='GrowthDes'>New Daily Tasks availabe to boost your rank</span>
+          <p id='GrowthButton'>Go Now →</p>
+        </div>
+        <div id='rank'>
+          <PiRankingLight id='RankIcon' />
+          <span>View LeaderBoard</span>
+          <span>See your ranking</span>
+          <span>See ranking →</span>
+        </div>
+        <div id='Impact'>
+          <LuTrees id='TreeIcon'/>
+          <span>My Impact</span>
+          <span>See the visual growth of your digital garden</span>
+          <span>Track Growth →</span>
         </div>
       </div>
     </>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -8,7 +8,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [authUser, setAuthUser] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const login = (name) => {
     setAuthUser(name);
