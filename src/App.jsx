620e6fb442360095f2ddb3971bd2284d14620d4a
Sundram1343 <sundramsharma2901@gmail.com>
2026-04-07 17:58:20 +0530

    footer error is removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
